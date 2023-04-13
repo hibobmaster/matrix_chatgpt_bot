75ff65a32aec20db427d1e16d3f439072f658eda
hibobmaster <bobmaster@hibobmaster.com>
2023-04-13 23:22:24 +0800

    Add Google's Bard Chat

--- BingImageGen.py
+++ BingImageGen.py
@@ -3,15 +3,12 @@ Code derived from:
 https://github.com/acheong08/EdgeGPT/blob/f940cecd24a4818015a8b42a2443dd97c3c2a8f4/src/ImageGen.py
 """
 from log import getlogger
-
-from typing import Union
 from uuid import uuid4
 import os
 import contextlib
 import aiohttp
 import asyncio
 import random
-import time
 import requests
 import regex
 
@@ -32,164 +29,6 @@ HEADERS = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.63",
     "x-forwarded-for": FORWARDED_IP,
 }
-
-# Error messages
-error_timeout = "Your request has timed out."
-error_redirect = "Redirect failed"
-error_blocked_prompt = (
-    "Your prompt has been blocked by Bing. Try to change any bad words and try again."
-)
-error_noresults = "Could not get results"
-error_unsupported_lang = "\nthis language is currently not supported by bing"
-error_bad_images = "Bad images"
-error_no_images = "No images"
-#
-sending_message = "Sending request..."
-wait_message = "Waiting for results..."
-download_message = "\nDownloading images..."
-
-
-def debug(debug_file, text_var):
-    """helper function for debug"""
-    with open(f"{debug_file}", "a") as f:
-        f.write(str(text_var))
-
-
-class ImageGen:
-    """
-    Image generation by Microsoft Bing
-    Parameters:3
-        auth_cookie: str
-    """
-
-    def __init__(
-        self, auth_cookie: str, debug_file: Union[str, None] = None, quiet: bool = False
-    ) -> None:
-        self.session: requests.Session = requests.Session()
-        self.session.headers = HEADERS
-        self.session.cookies.set("_U", auth_cookie)
-        self.quiet = quiet
-        self.debug_file = debug_file
-        if self.debug_file:
-            self.debug = partial(debug, self.debug_file)
-
-    def get_images(self, prompt: str) -> list:
-        """
-        Fetches image links from Bing
-        Parameters:
-            prompt: str
-        """
-        if not self.quiet:
-            print(sending_message)
-        if self.debug_file:
-            self.debug(sending_message)
-        url_encoded_prompt = requests.utils.quote(prompt)
-        # https://www.bing.com/images/create?q=<PROMPT>&rt=3&FORM=GENCRE
-        url = f"{BING_URL}/images/create?q={url_encoded_prompt}&rt=4&FORM=GENCRE"
-        response = self.session.post(url, allow_redirects=False)
-        # check for content waring message
-        if "this prompt has been blocked" in response.text.lower():
-            if self.debug_file:
-                self.debug(f"ERROR: {error_blocked_prompt}")
-            raise Exception(
-                error_blocked_prompt,
-            )
-        if (
-            "we're working hard to offer image creator in more languages"
-            in response.text.lower()
-        ):
-            if self.debug_file:
-                self.debug(f"ERROR: {error_unsupported_lang}")
-            raise Exception(error_unsupported_lang)
-        if response.status_code != 302:
-            # if rt4 fails, try rt3
-            url = f"{BING_URL}/images/create?q={url_encoded_prompt}&rt=3&FORM=GENCRE"
-            response3 = self.session.post(
-                url, allow_redirects=False, timeout=200)
-            if response3.status_code != 302:
-                if self.debug_file:
-                    self.debug(f"ERROR: {error_redirect}")
-                print(f"ERROR: {response3.text}")
-                raise Exception(error_redirect)
-            response = response3
-        # Get redirect URL
-        redirect_url = response.headers["Location"].replace("&nfy=1", "")
-        request_id = redirect_url.split("id=")[-1]
-        self.session.get(f"{BING_URL}{redirect_url}")
-        # https://www.bing.com/images/create/async/results/{ID}?q={PROMPT}
-        polling_url = f"{BING_URL}/images/create/async/results/{request_id}?q={url_encoded_prompt}"
-        # Poll for results
-        if self.debug_file:
-            self.debug("Polling and waiting for result")
-        if not self.quiet:
-            print("Waiting for results...")
-        start_wait = time.time()
-        while True:
-            if int(time.time() - start_wait) > 200:
-                if self.debug_file:
-                    self.debug(f"ERROR: {error_timeout}")
-                raise Exception(error_timeout)
-            if not self.quiet:
-                print(".", end="", flush=True)
-            response = self.session.get(polling_url)
-            if response.status_code != 200:
-                if self.debug_file:
-                    self.debug(f"ERROR: {error_noresults}")
-                raise Exception(error_noresults)
-            if not response.text or response.text.find("errorMessage") != -1:
-                time.sleep(1)
-                continue
-            else:
-                break
-        # Use regex to search for src=""
-        image_links = regex.findall(r'src="([^"]+)"', response.text)
-        # Remove size limit
-        normal_image_links = [link.split("?w=")[0] for link in image_links]
-        # Remove duplicates
-        normal_image_links = list(set(normal_image_links))
-
-        # bad_images = [
-        #     "https://r.bing.com/rp/in-2zU3AJUdkgFe7ZKv19yPBHVs.png",
-        #     "https://r.bing.com/rp/TX9QuO3WzcCJz1uaaSwQAz39Kb0.jpg",
-        # ]
-        # for img in normal_image_links:
-        #     if img in bad_images:
-        #         raise Exception("Bad images")
-        # No images
-        if not normal_image_links:
-            raise Exception(error_no_images)
-        return normal_image_links
-
-    def save_images(self, links: list, output_dir: str) -> str:
-        """
-        Saves images to output directory
-        """
-
-        # image name
-        image_name = str(uuid4())
-        # since matrix only support one media attachment per message, we just need one link
-        if links:
-            link = links.pop()
-
-        image_path = os.path.join(output_dir, f"{image_name}.jpeg")
-
-        with contextlib.suppress(FileExistsError):
-            os.mkdir(output_dir)
-        try:
-            with self.session.get(link, stream=True) as response:
-                # save response to file
-                response.raise_for_status()
-                with open(
-                    os.path.join(output_dir, image_path), "wb"
-                ) as output_file:
-                    for chunk in response.iter_content(chunk_size=8192):
-                        output_file.write(chunk)
-            return image_path
-        except requests.exceptions.MissingSchema as url_exception:
-            raise Exception(
-                "Inappropriate contents found in the generated images. Please try again or try another prompt.",
-            ) from url_exception
-
 
 class ImageGenAsync:
     """
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import asyncio
 import json
 import os
-import sys
-import asyncio
+
 from bot import Bot
 from log import getlogger
 
@@ -22,6 +22,7 @@ async def main():
                          api_key=config.get('api_key'),
                          bing_api_endpoint=config.get('bing_api_endpoint'),
                          access_token=config.get('access_token'),
+                         bard_token=config.get('bard_token'),
                          jailbreakEnabled=config.get('jailbreakEnabled'),
                          bing_auth_cookie=config.get('bing_auth_cookie'),
                          markdown_formatted=config.get('markdown_formatted'),
@@ -36,6 +37,7 @@ async def main():
                          api_key=os.environ.get("OPENAI_API_KEY"),
                          bing_api_endpoint=os.environ.get("BING_API_ENDPOINT"),
                          access_token=os.environ.get("ACCESS_TOKEN"),
+                         bard_token=os.environ.get("BARD_TOKEN"),
                          jailbreakEnabled=os.environ.get(
                              "JAILBREAKENABLED", "false").lower() in ('true', '1', 't'),
                          bing_auth_cookie=os.environ.get("BING_AUTH_COOKIE"),
