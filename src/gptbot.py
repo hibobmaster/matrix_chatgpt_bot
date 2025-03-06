9537950a6948e5c766873229029646e11067ce89
hibobmaster <32976627+hibobmaster@users.noreply.github.com>
2025-03-06 13:56:47 +0800

    :sparkles: add o1 and o3-mini model

--- src/gptbot.py
+++ src/gptbot.py
@@ -12,7 +12,7 @@ import tiktoken
 
 ENGINES = ["gpt-3.5-turbo", "gpt-4", "gpt-4-32k", "gpt-4-turbo"]
 
-GPT_O1_MODEL = ["o1-preview", "o1-mini"]
+GPT_O1_MODEL = ["o1-preview", "o1-mini", "o1", "o3-mini"]
 
 
 class Chatbot:
